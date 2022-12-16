d719220a7333f40590e3a0d178223d6f1b1ddd50
Alex <yuanxiang.wu.25@bishops.com>
2022-12-15 18:35:48 -0800

    Finished making all the pieces. Played with Forsyth–Edwards Notation

--- chess.js
+++ chess.js
@@ -56,41 +56,50 @@ class Piece {
   constructor(color) {
     this.color = color;
   }
+
+  insertMove(i, j, board, moves) {
+    if (board[i][j].piece == null) {
+      moves.push(board[i][j]);
+      return true;
+    }
+    if (board[i][j].piece.color != this.color)
+      moves.push(board[i][j]);
+    return false;
+  }
 }
 
 class Pawn extends Piece {
   constructor(color) {
     super(color);
-    this.hasMoved = false;
   }
 
   getMoves(i, j, board) {
     var moves = [];
     if (this.color == 'white') {
-      if (!this.hasMoved && board[i+1][j].piece == null && board[i+2][j].piece == null)
+      if (i == 1)
         moves.push(board[i+2][j]);
       if (board[i+1][j].piece == null)
         moves.push(board[i+1][j]);
       if (j+1 < board.length && board[i+1][j+1].piece != null && board[i+1][j+1].piece.color == 'black')
         moves.push(board[i+1][j+1]);
-      if (j-1 > 0 && board[i+1][j-1].piece != null && board[i+1][j-1].piece.color == 'black')
+      if (j-1 >= 0 && board[i+1][j-1].piece != null && board[i+1][j-1].piece.color == 'black')
         moves.push(board[i+1][j-1]);
     }
     else {
-      if (!this.hasMoved && board[i-1][j].piece == null && board[i-2][j].piece == null)
+      if (i == 6)
         moves.push(board[i-2][j]);
       if (board[i-1][j].piece == null)
         moves.push(board[i-1][j]);
       if (j+1 < board.length && board[i-1][j+1].piece != null && board[i-1][j+1].piece.color == 'white')
         moves.push(board[i-1][j+1]);
-      if (j-1 > 0 && board[i-1][j-1].piece != null && board[i-1][j-1].piece.color == 'white')
+      if (j-1 >= 0 && board[i-1][j-1].piece != null && board[i-1][j-1].piece.color == 'white')
         moves.push(board[i-1][j-1]);
     }
     return moves;
   }
 
   move() {
-    this.hasMoved = true;
+    return;
   }
 
   getStr() {
@@ -98,8 +107,179 @@ class Pawn extends Piece {
   }
 }
 
-class Rook extends Piece {
+class Queen extends Piece {
+  constructor(color) {
+    super(color);
+  }
 
+  getStr() {
+    return 'Queen';
+  }
+
+  getMoves(i, j, board) {
+    var moves = [];
+    for (var k=1; i+k<board.length&&j+k<board.length;k++)
+      if (!this.insertMove(i+k, j+k, board, moves))
+        break;
+    for (var k=1; i+k<board.length&&j-k>=0;k++)
+      if (!this.insertMove(i+k, j-k, board, moves))
+        break;
+    for (var k=1; i-k>=0&&j+k<board.length;k++)
+      if (!this.insertMove(i-k, j+k, board, moves))
+        break;
+    for (var k=1; i-k>=0&&j-k>=0;k++)
+      if (!this.insertMove(i-k, j-k, board, moves))
+        break;
+    for (var k=i+1; k<board.length; k++)
+      if (!this.insertMove(k, j, board, moves))
+        break;
+    for (var k=i-1; k>=0; k--)
+      if (!this.insertMove(k, j, board, moves))
+        break;
+    for (var k=j+1; k<board.length; k++)
+      if (!this.insertMove(i, k, board, moves))
+        break;
+    for (var k=j-1; k>=0; k--)
+      if (!this.insertMove(i, k, board, moves))
+        break;
+    return moves;
+  }
+
+  move() {
+    return;
+  }
+}
+
+class Bishop extends Piece {
+  constructor(color) {
+    super(color);
+  }
+
+  getStr() {
+    return 'Bishop';
+  }
+
+  getMoves(i, j, board) {
+    var moves = [];
+    for (var k=1; i+k<board.length&&j+k<board.length;k++)
+      if (!this.insertMove(i+k, j+k, board, moves))
+        break;
+    for (var k=1; i+k<board.length&&j-k>=0;k++)
+      if (!this.insertMove(i+k, j-k, board, moves))
+        break;
+    for (var k=1; i-k>=0&&j+k<board.length;k++)
+      if (!this.insertMove(i-k, j+k, board, moves))
+        break;
+    for (var k=1; i-k>=0&&j-k>=0;k++)
+      if (!this.insertMove(i-k, j-k, board, moves))
+        break;
+    return moves;
+  }
+
+  move() {
+    return;
+  }
+}
+
+
+class Rook extends Piece {
+  constructor(color) {
+    super(color);
+    this.hasMoved = false;
+  }
+
+  getStr() {
+    return 'Rook';
+  }
+
+  getMoves(i, j, board) {
+    var moves = [];
+    for (var k=i+1; k<board.length; k++)
+      if (!this.insertMove(k, j, board, moves))
+        break;
+    for (var k=i-1; k>=0; k--)
+      if (!this.insertMove(k, j, board, moves))
+        break;
+    for (var k=j+1; k<board.length; k++)
+      if (!this.insertMove(i, k, board, moves))
+        break;
+    for (var k=j-1; k>=0; k--)
+      if (!this.insertMove(i, k, board, moves))
+        break;
+    return moves;
+  }
+
+  move() {
+    this.hasMoved = true;
+  }
+
+}
+
+class Knight extends Piece {
+  constructor(color) {
+    super(color);
+  }
+
+  move() {
+    return;
+  }
+
+  getStr() {
+    return "Knight"
+  }
+
+  getMoves(i, j, board) {
+    var moves = [];
+    if (i+2 < board.length) {
+      if (j+1 < board.length)
+        this.insertMove(i+2, j+1, board, moves);
+      if (j-1 >= 0)
+        this.insertMove(i+2, j-1, board, moves);
+    }
+    if (i+1 < board.length) {
+      if (j+2 < board.length)
+        this.insertMove(i+1, j+2, board, moves);
+      if (j-2 >= 0)
+        this.insertMove(i+1, j-2, board, moves);
+    }
+    if (i-1 >= 0) {
+      if (j+2 < board.length)
+        this.insertMove(i-1, j+2, board, moves);
+      if (j-2 >= 0)
+        this.insertMove(i-1, j-2, board, moves);
+    }
+    if (i-2 >= 0) {
+      if (j+1 < board.length)
+        this.insertMove(i-2, j+1, board, moves);
+      if (j-1 >= 0)
+        this.insertMove(i-2, j-1, board, moves);
+    }
+    return moves;
+  }
+}
+
+class King extends Piece {
+  constructor(color) {
+    super(color);
+    this.hasMoved = false;
+  }
+
+  getStr() {
+    return "King";
+  }
+
+  move() {
+    this.hasMoved = true;
+  }
+
+  getMoves(i, j, board) {
+    var moves = [];
+    for (var y=-1; y<=1; y++)
+      for (var x=-1; x<=1; x++)
+        if (0 <= i+y < board.length && 0 <= j+x < board.length)
+          this.insertMove(i+y, j+x, board, moves);
+    return moves;
+  }
 }
 
 
@@ -166,10 +346,49 @@ class Board {
   }
 }
 
+function setupTile(i, j, s, board) {
+  if (s == 'p')
+    board[i][j].piece = new Pawn('black');
+  else if (s == 'P')
+    board[i][j].piece = new Pawn('white');
+  else if (s == 'n')
+    board[i][j].piece = new Knight('black');
+  else if (s == 'N')
+    board[i][j].piece = new Knight('white');
+  else if (s == 'b')
+    board[i][j].piece = new Bishop('black');
+  else if (s=='B')
+    board[i][j].piece = new Bishop('white');
+  else if (s == 'r')
+    board[i][j].piece = new Rook('black');
+  else if (s == 'R')
+    board[i][j].piece = new Rook('white');
+  else if (s == 'q')
+    board[i][j].piece = new Queen('black');
+  else if (s == 'Q')
+    board[i][j].piece = new Queen('white')
+  else if (s == 'k')
+    board[i][j].piece = new King('black')
+  else if (s == 'K')
+    board[i][j].piece = new King('white');
+}
+
 board = new Board();
-board.board[1][4].piece = new Pawn('white');
-for (var i=0; i<=7; i++)
-  board.board[i][i].piece = new Pawn('black');
-board.board[0][0].piece = new Pawn('white');
+var setupString = 'rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R';
+var tmp = setupString.split("/");
+console.log(tmp);
+for (var i=0; i<tmp.length; i++) {
+  var j = 0;
+  for (var k=0; k<tmp[0].length; k++) {
+    if (!Number.isNaN(parseInt(tmp[i][k]))) {
+      console.log('asd');
+      j += parseInt(tmp[i][k]);
+      continue;
+    }
+    setupTile(7-i, j, tmp[i][k], board.board);
+    j += 1;
+  }
+}
+console.log(board.board);
 board.update();
 c.addEventListener('click', board.selectTile.bind(board));
